79c8d6f8ee75bbd582d2ea4bc8d25cb8c6fca8f8
llun00 <trollun89@gmail.com>
2017-02-03 17:26:11 +0300

    fixed the issue

--- reverse.py
+++ reverse.py
@@ -1,15 +1,16 @@
 def ReverseS(str):
-str=list()
-spl_str=''
-n=len(str)-1
-while m<n
-  c = str[m]
-  str[m] = str[n]
-  str[n] = c
-  m=m+1
-  n=n-1
-return spl_str.join(str)
-
+    str=list(str)
+    spl_str=''
+    n=len(str)-1
+    m=0
+    while m<n:
+        c = str[m]
+        str[m] = str[n]
+        str[n] = c
+        m=m+1
+        n=n-1
+    return spl_str.join(str)
+    
 s=raw_input('Enter a string: ')
 s=ReverseS(s)
 print(s)
